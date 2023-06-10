06e6ef7593b307f5ebc70c3eb939bdf52eb70cbc
Pham Duc Tho <20041501+thopd88@users.noreply.github.com>
2023-06-10 15:42:49 +0700

    Laravel API

---
+++ Book/app/Http/Controllers/api/BookController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return $books;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $book = new Book();
+
+        $book->title = $request->title;
+        $book->author_id = $request->author_id;
+        $book->category_id = $request->category_id;
+        $book->description = $request->description;
+
+        $book->save();
+
+        return $book;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
